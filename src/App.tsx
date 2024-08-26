4b32b63ceff6069a3a3cdaf7d67aa8b46f04884d
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-26 16:33:07 -0300

    refactor: change classe name and title and remove space

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import { Hierarchy } from './components/Hierarchy';
 import { ExportButton } from './components/ExportButton';
 import { ItemProvider } from './hooks/useItem';
 
-
 function App() { 
   return (
     <ItemProvider>
       <div className='container'>
-        <div className='hierarchy-group'>
-          <h1 className='hierarchy-title'>Hierarquia de Palavras</h1>
+        <div className='hierarchy__group'>
+          <h1 className='hierarchy__title'>Hierarquia</h1>
           <ExportButton />
         </div>
         <Hierarchy />
