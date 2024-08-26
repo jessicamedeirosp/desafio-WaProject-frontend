14efc25c430644c09676e811bc4a090b31d1061c
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-08-26 17:55:27 -0300

    refactor: adding uuid

--- src/hooks/useItem.tsx
+++ src/hooks/useItem.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from "react"
 import { IItem } from "../interfaces/Item"
+import { v4 as uuidv4 } from 'uuid';
 
 interface ItemContextProps {
   items: IItem[]
@@ -16,19 +17,19 @@ const ItemContext = createContext<ItemContextProps>({} as ItemContextProps)
 export function ItemProvider({ children }: ItemProviderProps) {
   const initialItems: IItem[] = [
     {
-      id: '1',
+      id: 'f3992e31-1709-4f1e-a0cd-22b3623bb10f',
       name: 'Animais',
       children: [
         {
-          id: '2',
+          id: 'dd289241-8614-4c63-a5f2-6bbc47adc968',
           name: 'Mamíferos',
           children: [
-            { id: '3', name: 'Carnívoros', children: [] },
-            { id: '4', name: 'Herbívoros', children: [] }
+            { id: 'b2ec6f43-a0a2-4e56-9490-d5272acbb641', name: 'Carnívoros', children: [] },
+            { id: '082b9d36-987f-4d15-a2ea-68f8f7e78fd1', name: 'Herbívoros', children: [] }
           ]
         },
         {
-          id: '5',
+          id: 'ecc3fa7c-b679-4700-81de-4088016bc500',
           name: 'Aves',
           children: []
         }
@@ -40,7 +41,7 @@ export function ItemProvider({ children }: ItemProviderProps) {
 
   function addItem(parentId?: string) {
     const newItem: IItem = {
-      id: Date.now().toString(),
+      id: uuidv4(),
       name: '',
       children: []
     };
